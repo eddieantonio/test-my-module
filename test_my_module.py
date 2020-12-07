baa4443c8c7621a8cf87000ab91394b668c83e44
Eddie Antonio Santos <easantos@ualberta.ca>
2020-12-06 21:00:37 -0700

    change: collect_test on a dictionary (global object)

--- test_my_module.py
+++ test_my_module.py
@@ -1,6 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+import inspect
+
+
 """
 test-my-module
+
+    def hi():
+        return "hello"
+
+    def test():
+        assert hi() == "hello"
+
+    if __name__ == "__main__":
+        import test_my_module
+        test_my_module.run_all_tests()
 """
+
+
+def run_all_tests():
+    """
+    Runs all tests in the module that called it.
+    """
+
+    # figure out who called us
+    caller = inspect.currentframe().f_back
+    try:
+        namespace = caller.f_globals
+    finally:
+        del caller
+
+    test_cases = collect_tests(namespace)
+    print(list(test_cases))
+
+
+def collect_tests(global_object):
+    """
+    Yield possible test functions: (name, callable) pairs.
+    """
+
+    for name in global_object:
+        if not name.startswith("test"):
+            continue
+
+        fn = global_object[name]
+
+        if callable(fn):
+            yield name, fn
--- test_test_my_module.py
+++ test_test_my_module.py
@@ -4,5 +4,25 @@
 import test_my_module
 
 
-def test_nothing():
-    assert True
+def test_collect_tests_from_dict():
+    def inc(x):
+        return x + 1
+
+    def test_answer():
+        assert inc(3) == 5
+
+    def test():
+        assert 2 + 2 == 4
+        assert 4 - 1 == 3, "quick maths"
+
+    global_object = {
+        "test": test,
+        "inc": inc,
+        "test_answer": test_answer,
+    }
+
+    all_tests = list(test_my_module.collect_tests(global_object))
+    assert len(all_tests) == 2
+    for name, fn in all_tests:
+        assert name.startswith("test")
+        assert callable(fn)
